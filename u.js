bff72555474459ed34b64fd63cb709f4d329614f
Володя Колесников <voloko@gmail.com>
2011-07-18 11:11:57 -0700

    base view

--- u.js
+++ u.js
@@ -5,6 +5,8 @@
 */
 var u = {};
 
+
+// MISC
 var trans = {
   '&': '&amp;',
   '<': '&lt;',
@@ -18,10 +20,31 @@ function escapeHTML(html) {
 
 u.escapeHTML = escapeHTML;
 
-function _cleanCls(result) {
-  return result.trim().replace(/\s{2,}/g, ' ');
-}
+u.extend = function() {
+  var target = arguments[0] || {};
+  var i = 1;
+  var length = arguments.length;
+  var options;
+  for (; i < length; i++) {
+    if (options = arguments[i]) {
+      for (var name in options) {
+        if (!options.hasOwnProperty(name)) {
+          continue;
+        }
+        var copy = options[name];
 
+        if (copy !== undefined) {
+          target[name] = copy;
+        }
+      }
+    }
+  }
+  return target;  
+};
+
+
+
+// className
 var cls = u.cls = {
   has: function(elem, className) {
     return (' ' + elem.className + ' ').indexOf(' ' + className + ' ') > -1;
@@ -53,5 +76,62 @@ var cls = u.cls = {
     condition ? cls.add(elem, className) : cls.remove(elem, className);
   }
 };
+
+function _cleanCls(result) {
+  return result.trim().replace(/\s{2,}/g, ' ');
+}
+
+
+
+// Props
+u.delegate = {
+  prop: function(obj, propName, toObjName, toPropName, propDef) {
+    toPropName = toPropName || propName;
+    Object.defineProperty(obj, propName, u.extend({
+      configurable: true,
+      enumerable: true,
+      set: function(value) {
+        this[toObjName][toPropName] = value;
+      },
+      get: function() {
+        return this[toObjName][toPropName];
+      }
+    }, propDef));
+  },
+  call: function(obj, funcName, toObjName, toFuncName) {
+    toFuncName = toFuncName || funcName;
+    obj[funcName] = function() {
+      return this[toObjName][toFuncName].apply(this[toObjName], arguments);
+    };
+  }
+};
+
+
+
+// Types
+u.is = {
+  view: function(object) {
+    return !!(object && object.isView);
+  },
+  element: function(object) {
+    return !!(object && object.nodeType == 1);
+  }
+}
+
+([
+  ['func',   'Function'],
+  ['array',  'Array'],
+  ['bool',   'Boolean'],
+  ['number', 'Number'],
+  ['string', 'String'],
+  ['date',   'Date'],
+  ['number', 'Number']
+]).forEach(function(pair) {
+  var typeName = '[object ' + pair[1] + ']';
+  u.is[pair[0]] = function(object) {
+    return Object.prototype.toString.call(object) === typeName;
+  };
+});
+  
 
 module.exports = u;
--- v.js
+++ v.js
@@ -7,23 +7,14 @@ var u = require('./u');
 var _toString = Object.prototype.toString;
 
 function v(markup, refs) {
-  if (markup.vuew) {
-    return createView(markup, refs);
-  } if (markup.tag) {
-    return createElement(markup, refs);
+  if (u.is.view(markup) || u.is.element(markup)) {
+    return markup;
   }
-}
-
-function createView(markup, refs) {
-  var cls = resolveView(markup.view);
-}
-
-function resolveView(view) {
   
-}
-
-function createElement(markup, refs) {
-  var element = document.createElement(markup.tag);
+  var item = markup.view ? 
+    createView(markup) : 
+    createElement(markup);
+    
   for (var key in markup) {
     var value = markup[key];
     switch (key) {
@@ -32,26 +23,118 @@ function createElement(markup, refs) {
       case 'as':
         refs && (refs[key] = value);
         break;
+      case 'aslist':
+        if (refs) {
+          refs[key] ? (refs[key] = [value]) : refs[key].push(value);
+        }
+        break;
       case 'children':
         value.forEach(function(m) {
-          element.appendChild(d(m, refs));
+          var child = d(m, refs);
+          if (!markup.view && u.is.view(child)) {
+            child.attachTo(item);
+          } else {
+            item.appendChild(child);
+          }
         });
         break;
       case 'text':
-        element.appendChild(document.createTextNode(value));
+        item.appendChild(document.createTextNode(value));
         break;
       case 'className':
       case 'innerHTML':
-        element[key] = value;
+        item[key] = value;
         break;
       default:
-        element.setAttribute(key, value);
+        item.setAttribute(key, value);
     }
   }
   return element;
 }
 
-var View = {};
+function _createView(markup) {
+  var cls = u.is.string(markup.view) ? v[markup.view] : markup.view;
+  return new cls(markup);
+}
+
+function _createElement(markup) {
+  var element = document.createElement(markup.tag);
+}
+
+v.nearest = function(element) {
+  while (element) {
+    if (element['data-view']) { return element['data-view']; }
+    element = element.parentNode;
+  }
+  return null;  
+};
+
+
+var Base = v.Base = function(markup) {
+  this._setup(markup);
+  this._createDom();
+  this._init();
+};
+var bp = Base.prototype;
+Object.defineProperty(bp, 'isObject', { 
+  value: true, 
+  configurable: true,
+  enumerable: true });
+Object.defineProperty(bp, 'dom', { 
+  writable: true, 
+  configurable: true,
+  enumerable: true });
+u.delegate.prop(bp, 'className', 'dom');
+u.delegate.prop(bp, 'parentNode', 'dom');
+u.delegate.prop(bp, 'children', 'dom');
+u.delegate.prop(bp, 'innerHTML', 'dom');
+u.delegate.prop(bp, 'style', 'dom', 'style', { get: undefined });
+
+bp._setup = function(markup) {};
+
+bp._createDom = function() {
+  this.dom = v({ tag: 'div' });
+};
+
+bp._init = function() {
+  this.dom['data-view'] = this;
+};
+
+bp.appendChild = function(child) {
+  this.dom.appendChild(child.dom || child);
+  return child;
+};
+
+bp.removeChild = function(child) {
+  this.dom.removeChild(child.dom || child);
+  return child;
+};
+
+bp.insertBefore = function(child, refChild) {
+  this.dom.insertBefore(child.dom || child, refChild.dom, refChild);
+  return child;
+};
+
+bp.replaceChild = function(child, refChild) {
+  this.dom.replaceChild(child.dom || child, refChild.dom, refChild);
+  return child;
+};
+
+Object.defineProperty(bp, 'parentView', { 
+  configurable: true,
+  enumerable: true,
+  get: function() {
+    return v.nearest(this.parentNode);
+  } });
+  
+Object.defineProperty(bp, 'childViews', { 
+  configurable: true,
+  enumerable: true,
+  get: function() {
+    return this.children.filter(function(child) {
+      return u.is.view(child);
+    });
+  } });
 
 
 module.exports = v;
